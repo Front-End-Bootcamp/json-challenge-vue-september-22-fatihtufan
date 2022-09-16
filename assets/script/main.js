f78ca04bed0711afb50699f11b79a0b49adf7ab7
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2022-09-17 00:04:06 +0300

    week 1 json challenge completed

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,53 @@
-console.log('👻');
+
+import data from '../../data/data.json';
+
+
+function getByGroupName(groupName,data){
+    let isThere = data.find(obj => obj.group === groupName); // Gelen verinin data içerisinde olup/olmamasına bakıldı
+    if (isThere){
+        let groupData=data.filter(function (data){
+            return data.group===groupName;
+        });
+       
+        let assistant=groupData.filter(function(groupData){
+            return groupData.type=="assistant";
+        });
+        
+        let students=groupData.filter(function(groupData){
+            return groupData.type==null; 
+        });
+
+        let names=Array();
+        students.forEach(element => {
+            names.push(element.name);
+        });
+        return createObject(groupName,names,assistant[0].name);
+    }
+    return  "Not found in data";
+}
+
+function createObject(groupName,person,assistant){
+    let group={
+        group:groupName,
+        student:person,
+        assistant:assistant
+    };
+    return JSON.stringify(group); //Sonuçlar json formatına çevrildi
+}
+
+function getGroups(data){
+    let groupNames=Array();
+    data.forEach(element=>{
+        let check=groupNames.find(i=>i==element.group);
+        if(check==undefined){
+            groupNames.push(element.group);
+        }
+    });
+
+    groupNames.forEach(element => {
+        console.log(getByGroupName(element,data));
+    });
+}
+console.log(getByGroupName("SteelBlue",data));
+console.log("👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻");
+getGroups(data);
